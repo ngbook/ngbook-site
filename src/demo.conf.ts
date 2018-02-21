21f4fe8e566955c6a81b6014f422ff9990ad6ada
jsongo <jsongo@qq.com>
2018-02-21 21:02:44 +0800

    update

--- src/demo.conf.ts
+++ src/demo.conf.ts
@@ -3,61 +3,76 @@ export const demos = [
     {
         title: '1、Tooltip示例',
         pic: './assets/imgs/screenshots/1.png',
+        link: 'https://ngbook.js.org/tooltip/',
     },
     {
         title: '2、图片列表示例',
         pic: './assets/imgs/screenshots/2.png',
+        link: 'https://ngbook.js.org/pic-list/',
     },
     {
         title: '3、滚轮倒计时',
         pic: './assets/imgs/screenshots/3.png',
+        link: 'https://ngbook.js.org/countdown/',
     },
     {
         title: '4、Pipe管道演示随机图片',
         pic: './assets/imgs/screenshots/4.png',
+        link: 'https://ngbook.js.org/pipe-random/',
     },
     {
         title: '5、简单Animation动画测试',
         pic: './assets/imgs/screenshots/5.png',
+        link: 'https://ngbook.js.org/animation1/',
     },
     {
         title: '6、Animation进出动画演示',
         pic: './assets/imgs/screenshots/6.png',
+        link: 'https://ngbook.js.org/animation2/',
     },
     {
         title: '7、Animation进出动画升级版',
         pic: './assets/imgs/screenshots/7.png',
+        link: 'https://ngbook.js.org/animation3/',
     },
     {
         title: '8、Angular官方的Animation动画示例',
         pic: './assets/imgs/screenshots/8.png',
+        link: 'https://ngbook.js.org/angular-animation/',
     },
     {
         title: '9、简单的form表单演示',
         pic: './assets/imgs/screenshots/9.png',
+        link: 'https://ngbook.js.org/form-basic/',
     },
     {
         title: '10、网络请求模块的封装示例',
         pic: './assets/imgs/screenshots/10.png',
+        link: 'https://ngbook.js.org/net/',
     },
     {
         title: '11、分页组件演示（加缓存）',
         pic: './assets/imgs/screenshots/11.png',
+        link: 'https://ngbook.js.org/pager/',
     },
     {
         title: '12、模态框及各种弹窗',
         pic: './assets/imgs/screenshots/12.png',
+        link: 'https://ngbook.js.org/modal/',
     },
     {
         title: '13、简单路由功能演示',
         pic: './assets/imgs/screenshots/13.png',
+        link: 'https://ngbook.js.org/simple-router/',
     },
     {
         title: '14、路由Outlet功能演示',
         pic: './assets/imgs/screenshots/14.png',
+        link: 'https://ngbook.js.org/router-outlet/',
     },
     {
         title: '15、RxJS — switchMap功能演示',
         pic: './assets/imgs/screenshots/15.png',
+        link: 'https://ngbook.js.org/switchMap/',
     },
 ];
